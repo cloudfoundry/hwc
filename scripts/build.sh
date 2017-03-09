917fe5fd19c6a755a1c6ba81703f4fee9c835c20
Sunjay Bhatia <sunjayBhatia@users.noreply.github.com>
2017-03-08 19:01:57 -0500

    make bin dir in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@
 ROOTDIR="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )"
 BINDIR=$ROOTDIR/bin
 
+mkdir -p $BINDIR
+
 set -ex
 
 GOOS=windows go build -o $BINDIR/hwc.exe github.com/cloudfoundry-incubator/hwc/hwc
