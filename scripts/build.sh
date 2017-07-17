b8f090bed5fdffb5e6b7688cde1a01d5b5f6364c
Sam Smith <sesmith177@gmail.com>
2017-07-17 15:38:36 -0400

    Update build script to use CGO and external linker
    [#146408413]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 set -ex
 
 mkdir -p $PWD/hwc-rel
-GOOS=windows go build -o $PWD/hwc-rel/hwc.exe code.cloudfoundry.org/hwc
+CGO_ENABLED=1 GO_EXTLINK_ENABLED=1 CC="x86_64-w64-mingw32-gcc" GOOS=windows go build -o $PWD/hwc-rel/hwc.exe code.cloudfoundry.org/hwc
