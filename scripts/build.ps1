37518be30466901c4a89b19c811aca3f77540815
Wael Rabadi <wael@mwrabadi.com>
2020-03-24 14:45:54 -0500

    support win32 with native call added webapp with native DLL call

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,8 +4,15 @@ if (-not (Test-Path env:GOPATH)) {
 }
 
 New-Item -path "$PWD/hwc-rel" -type directory -force
+
 $env:CGO_ENABLED=1
 $env:GO_EXTLINK_ENABLED=1
+# x64
 $env:CC="x86_64-w64-mingw32-gcc"
-
+$env:GOARCH="amd64"
 go build -o $PWD/hwc-rel/hwc.exe code.cloudfoundry.org/hwc
+
+# Win32
+$env:CC="i686-w64-mingw32-gcc"
+$env:GOARCH="386"
+go build -o $PWD/hwc-rel/hwc_x86.exe code.cloudfoundry.org/hwc
