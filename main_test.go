2a217f29d35f8246a0c93c0463a0f8a7703a1d66
Victoria Henry <vhenry@vmware.com>
2021-04-13 23:37:41 -0400

    wip; 404s

--- main_test.go
+++ main_test.go
@@ -89,14 +89,14 @@ var _ = Describe("HWC", func() {
 			Eventually(app.session).Should(gbytes.Say("Server Started"))
 		})
 
-		//AfterEach(func() {
-		//	stopApp(app)
-		//	Eventually(app.session).Should(gexec.Exit(0))
-		//	Eventually(app.session).Should(gbytes.Say("Server Shutdown"))
-		//})
+		AfterEach(func() {
+			stopApp(app)
+			Eventually(app.session).Should(gexec.Exit(0))
+			Eventually(app.session).Should(gbytes.Say("Server Shutdown"))
+		})
 
 		It("does static compression", func() {
-			staticUrl := fmt.Sprintf("http://localhost:%d/Content/bootstrap.css", app.port)
+			staticUrl := fmt.Sprintf("https://localhost:%d/Content/bootstrap.css", app.port)
 			var header http.Header
 
 			//the app needs more than one request in order to do the compression
