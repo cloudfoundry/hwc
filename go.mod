d1388227a9c12ab78f24df9f11710ca373e92dac
Victoria Henry <vhenry@vmware.com>
2021-04-15 12:51:32 -0400

    wip; go test

--- go.mod
+++ go.mod
@@ -5,10 +5,10 @@ go 1.14
 require (
 	github.com/cloudfoundry-community/go-cfenv v1.17.1-0.20171115121958-e84b5c116637
 	github.com/mitchellh/mapstructure v0.0.0-20180111000720-b4575eea38cc
-	github.com/onsi/ginkgo v1.4.1-0.20180118182312-7354a07ba455
-	github.com/onsi/gomega v1.3.1-0.20180130223036-4fc17627b66f
-	golang.org/x/net v0.0.0-20180112015858-5ccada7d0a7b
-	golang.org/x/sys v0.0.0-20180117170059-2c42eef0765b
-	golang.org/x/text v0.3.1-0.20171227012246-e19ae1496984
-	gopkg.in/yaml.v2 v2.0.0
+	github.com/onsi/ginkgo v1.16.1
+	github.com/onsi/gomega v1.10.1
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974
+	golang.org/x/sys v0.0.0-20210112080510-489259a85091
+	golang.org/x/text v0.3.3
+	gopkg.in/yaml.v2 v2.3.0
 )
--- scripts/run_test.ps1
+++ scripts/run_test.ps1
@@ -1,20 +1,15 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-Install-WindowsFeature Web-WHC
-Install-WindowsFeature Web-Webserver
-Install-WindowsFeature Web-WebSockets
-Install-WindowsFeature Web-WHC
-Install-WindowsFeature Web-ASP
-Install-WindowsFeature Web-ASP-Net45
+Install-WindowsFeature Web-WHC, Web-Webserver, Web-WebSockets, Web-ASP, Web-ASP-Net45
 
-Write-Host "Installing Ginkgo"
-go.exe get github.com/onsi/ginkgo/ginkgo
-if ($LastExitCode -ne 0) {
-    throw "Ginkgo installation process returned error code: $LastExitCode"
-}
+# Write-Host "Installing Ginkgo"
+# go.exe get github.com/onsi/ginkgo/ginkgo
+# if ($LastExitCode -ne 0) {
+#     throw "Ginkgo installation process returned error code: $LastExitCode"
+# }
 
-~/go/bin/ginkgo.exe -r -race -keepGoing -p
+go test -v -count=1 -parallel=1 ./...
 if ($LastExitCode -ne 0) {
     throw "Testing hwc returned error code: $LastExitCode"
 }
